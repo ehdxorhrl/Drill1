aac0a82da0329627357e5803632fa3b153d07dc1
ehdxorhrl <leedj6477@naver.com>
2023-09-15 18:39:42 +0900

    Drill 1 제출(2020182029 이동재)
    Drill1 py파일 제출

---
+++ turtle_control.py
@@ -0,0 +1,35 @@
+import turtle
+
+def turtle_up():
+    turtle.seth(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_left():
+    turtle.seth(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_right():
+    turtle.seth(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_down():
+    turtle.seth(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def turtle_restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(turtle_up,'w')
+turtle.onkey(turtle_left,'a')
+turtle.onkey(turtle_right,'d')
+turtle.onkey(turtle_down,'s')
+turtle.onkey(turtle_restart,'Escape')
+turtle.listen()
+    
+    
